dbd98b0c1f9adbc9e002a84ab7d2da9b608434dd
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-10-14 09:45:16 +0200

    creating structure for process

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import speech_recognition as sr
-from pydub import AudioSegment
 
-filename = "Resources/Myname.wav"
 
-# convert file format
-# wav_fileformat = r"Resources\file1.wav"
+def audioTOtext():
+    # Access file
+    src = "Resources/Recording.wav"
 
-# Recognition process
-recognition = sr.Recognizer()
+    # Recognition process
+    recognition = sr.Recognizer()
 
-# Access file
-with sr.AudioFile(filename) as source:
-    # Audio to memory
-    audio_data = recognition.record(source)
-    # Audio to text
-    text = recognition.recognize_google(audio_data)
-    print(text)
+    # Access file
+    with sr.AudioFile(src) as source:
+        # Audio to memory
+        audio_data = recognition.record(source)
+        # Audio to text
+        text = recognition.recognize_google(audio_data)
+        print(text)
+
+
+audioTOtext()
