1a4584393b3687b988415806f1f21d228aa811af
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-10-26 16:36:16 +0200

    using BERT for the sentiment analysis

--- BERT.py
+++ BERT.py
@@ -2,8 +2,10 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 import re
 
 
 tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
 model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
+
