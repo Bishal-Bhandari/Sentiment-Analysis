8cec402ffdd5e2f2ecd353aa3461bb843226244b
Bishal-Bhandari <bishalbhaandari62@gmail.com>
2022-10-26 18:02:43 +0200

    data is saved into the file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import LiveInput
 import BERT
+from openpyxl import load_workbook
 
 # Recognition process
 recognition = sr.Recognizer()
@@ -17,7 +18,14 @@ def audioTotext():
         src_file_data = recognition.record(source)
         # Audio to text
         audio_to_text = recognition.recognize_google(src_file_data)
+
+        # using bert module
         score_bert = bertObj.bert_analysis(audio_to_text)
+
+        # passing value to save into the file
+        File_Work(audio_to_text, score_bert)
+
+        # final output
         print(audio_to_text + '\n Analysied score:' + score_bert)
 
 
@@ -27,8 +35,29 @@ def LiveVoice():
     live_audio_data = LiveObj.TakeAudio()
     # feeding data to recognition process
     audio_to_text = recognition.recognize_google(live_audio_data)
+
+    # using bert module
     score_bert = bertObj.bert_analysis(audio_to_text)
+
+    # passing value to save into the file
+    File_Work(audio_to_text, score_bert)
+
+    # final output
     print(audio_to_text + '\n Analysied score:' + score_bert)
+
+
+def File_Work(audioData, audioScore):
+    analyzed_data = []
+    tem_list = [audioData, audioScore]
+    analyzed_data.append(tem_list)
+    # using excel file to save data
+    Sheet_Name = 'RecordFile.xlsx'
+    Data_File = load_workbook(Sheet_Name)
+    page = Data_File.active
+    # New data to write
+    for info in analyzed_data:
+        page.append(info)
+    Data_File.save(filename=Sheet_Name)
 
 
 print('\x1b[6;30;42m' + 'WELCOME' + '\x1b[0m')
